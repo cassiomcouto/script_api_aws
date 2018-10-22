c4fc351daccbfd5e53098ffd6b7bf425a6cdd67e
Cássio Couto <cassio.couto@chaordicsystems.com>
2018-10-22 12:07:00 -0300

    correction of array values

--- list_kernel_ec2.rb
+++ list_kernel_ec2.rb
@@ -4,29 +4,29 @@ require 'net/ssh'
 #Declarar usuario ssh
 @username = "cassiocouto"
 
-verify_itype=['c5.', 'c5d.', 'i3', 'm5', 'm5d', 'r5', 'r5d', 't3']
+verify_itype=['c5', 'c5d', 'i3', 'm5', 'm5d', 'r5', 'r5d', 't3']
 
 ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
-cont_instancia=0
+cont_instance=0
 cont_notaccess=0
 #Lista todas as instancias
-ec2.instances.each do |i|
- if verify_itype.include? i.instance_type.split(".").first
+ec2.instances.each do |instance|
+ if verify_itype.include? instance.instance_type.split(".").first
   begin
    puts "------------"
 
-   i.tags.each do |tag|
+   instance.tags.each do |tag|
     if tag.key == "Name"
      @name = tag.value
       end
    end
    
    puts "Name: #{@name}"
-   puts "Type: #{i.instance_type}"
-   cont_instancia = cont_instancia + 1
+   puts "Type: #{instance.instance_type}"
+   cont_instance = cont_instance + 1
    
    #Acessa a maquina por ssh
-   Net::SSH.start("#{i.private_ip_address}", "#{@username}") do |ssh|
+   Net::SSH.start("#{instance.private_ip_address}", "#{@username}") do |ssh|
      @kernel = ssh.exec!("uname -r")
    end
   
@@ -41,5 +41,5 @@ ec2.instances.each do |i|
  end
 end 
  puts "-------------------------------------------------------" 
- puts "Total de instancia do typo informado: #{cont_instancia}"
- puts "Total de instancia erro: #{cont_notaccess}"
+ puts "Total de instancias: #{cont_instance}"
+ puts "Total de instancias com erro de acesso: #{cont_notaccess}"
